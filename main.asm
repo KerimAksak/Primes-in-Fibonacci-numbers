baca84a7e3b9b81243bfb2060d46cc201ac25880
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-05-10 23:25:03 +0300

    User input function added

--- main.asm
+++ main.asm
@@ -1,91 +1,72 @@
-name "hi-world"
-
-; this example prints out  "hello world!"
-; by writing directly to video memory.
-; in vga memory: first byte is ascii character, byte that follows is character attribute.
-; if you change the second byte, you can change the color of
-; the character even after it is printed.
-; character attribute is 8 bit value,
-; high 4 bits set background color and low 4 bits set foreground color.
-
-; hex    bin        color
-; 
-; 0      0000      black
-; 1      0001      blue
-; 2      0010      green
-; 3      0011      cyan
-; 4      0100      red
-; 5      0101      magenta
-; 6      0110      brown
-; 7      0111      light gray
-; 8      1000      dark gray
-; 9      1001      light blue
-; a      1010      light green
-; b      1011      light cyan
-; c      1100      light red
-; d      1101      light magenta
-; e      1110      yellow
-; f      1111      white
- 
-
-
-org 100h
-
-; set video mode    
-mov ax, 3     ; text mode 80x25, 16 colors, 8 pages (ah=0, al=3)
-int 10h       ; do it!
-
-; cancel blinking and enable all 16 colors:
-mov ax, 1003h
-mov bx, 0
-int 10h
-
-
-; set segment register:
-mov     ax, 0b800h
-mov     ds, ax
-
-; print "hello world"
-; first byte is ascii code, second byte is color code.
-
-mov [02h], 'H'
-
-mov [04h], 'e'
-
-mov [06h], 'l'
-
-mov [08h], 'l'
-
-mov [0ah], 'o'
-
-mov [0ch], ','
-
-mov [0eh], 'W'
- 
-mov [10h], 'o'
-
-mov [12h], 'r'
-
-mov [14h], 'l'
-
-mov [16h], 'd'
-
-mov [18h], '!'
+;name "hi-world"
+   
+; macros
+WRITE_STRING MACRO string_offset
+    MOV DX, OFFSET user_message
+    MOV AH, 09H
+    INT 21H
+    ENDM ; end macro 
+; end of macros
 
 
 
+; start main
+org 100h  
 
-; color all characters:
-mov cx, 12  ; number of characters.
-mov di, 03h ; start from byte after 'h'
+.data
+    user_message DB 'Please give me a number...: ','$'
 
-c:  mov [di], 11101100b   ; light red(1100) on yellow(1110)
-    add di, 2 ; skip over next ascii code in vga memory.
-    loop c
-
-; wait for any key press:
-mov ah, 0
-int 16h
+.code
+    
+    WRITE_STRING user_message
+    
+    MOV AH, 1H  ; keyboard input subprogram
+    INT 21H     ; read character into AL  
+    MOV AH, 1H  ; keyboard input subprogram
+    INT 21H     ; read character into AL
+    
+    CALL ENDL   ; new line process
+    
+    MOV AH, 2H      ; character output subprogram 
+    MOV DL, AL      ; copy character to DL 
+    INT 21H         ; display character in DL 
+    MOV AH, 2H      ; character output subprogram 
+    MOV DL, AL      ; copy character to DL 
+    INT 21H         ; display character in DL
+    
+    
+       
 
 ret
+; end of main
 
+
+
+
+; start process
+ENDL PROC NEAR      ; a new line function
+    PUSH DX         ; don't lose values
+    PUSH AX         ; don't lose values
+    MOV DL, 0DH     ; \r
+    MOV AH, 02H  
+    INT 21h  
+    MOV DL, 0AH     ; \n
+    MOV AH, 02H
+    INT 21H 
+    POP AX
+    POP DX
+    RET
+ENDL ENDP           ; end process     
+
+
+ASCII2INT PROC NEAR ; ascii convert to int value
+    PUSH DX
+    PUSH AX
+          
+    
+          
+    POP AX
+    POP DX 
+    
+; end of process
+                                   
