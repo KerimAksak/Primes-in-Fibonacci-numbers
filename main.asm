038bf66685fe0c86f5c748acfd149bd24d9f626e
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-05-20 19:32:44 +0300

    Converting the entered number to hexadecimal.
    The result of the operations is controlled by the stack structure.

--- main.asm
+++ main.asm
@@ -3,6 +3,7 @@
 ; INPUT RULE: Period must be added for each digit of numbers.
 ; EXAMPLE INPUT: 1.123
 DATA SEGMENT
+    DIGIT DW 0 ; Number of digits 
     MSG1 DB 10,13,"Please enter a number (Max: 65.536 <2^16>)...: $"
     MSG2 DB 10,13,"ERROR! <Two points cannot be made one after the other.> $"   
     MSG3 DB 10,13,"Control message... $"
@@ -43,7 +44,10 @@ START:
         
     above2E: ; ASCII(0-9 number) = 30-39 / If the number is entered, proceed without checking
         MOV BYTE PTR [BP-20], AL
-        SUB AL,30H
+        SUB AL,30H ; convert ASCII to decimal
+        INC DIGIT  ; digit ++
+        XOR AH, AH ; clear AH
+        PUSH AX
         LOOP label
 
     equalPoint: ; If there are two dots, show an error message and end to process
@@ -56,9 +60,30 @@ START:
         MOV AH,9
         INT 21H
         JMP continue
-        ; programin devami burda olmali
+        ; The continuation of the program should be here
         
     continue: ; End of user input and next process.(fibo) 
-                
+        MOV CX, DIGIT
+        MOV DI, 1 ; Let's start directly at 10^1. Then we add the value 10^0 to the result.
+         
+        MOV BYTE PTR [BP-28],1
+        MOV DI,1
+        JMP convertToHex
+        
+    convertToHex: ; Since the entered numbers consist of a single digit in decimal, 
+                  ;  we keep this by converting to hexadecimal.
+        
+        MOV CX,WORD PTR [BP-28]
+        
+        MOV AX,10
+        exponential: ; Loops according to the value of 10^n.
+            MUL DI 
+            LOOP exponential
+            
+        MOV DI,10
+        INC BYTE PTR [BP-28]
+        JMP convertToHex
+
+                    
 END START
                                    
