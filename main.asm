56e32c168371fc8471e6770f3395afef6eb4afba
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-05-20 18:04:13 +0300

    Enter(key) value control

--- main.asm
+++ main.asm
@@ -5,6 +5,7 @@
 DATA SEGMENT
     MSG1 DB 10,13,"Please enter a number (Max: 65.536 <2^16>)...: $"
     MSG2 DB 10,13,"ERROR! <Two points cannot be made one after the other.> $"   
+    MSG3 DB 10,13,"Control message... $"
 ENDS
 
 
@@ -26,15 +27,20 @@ START:
         ; into data <data is in AL>
         MOV AH,1
         INT 21H
+        
+        CMP AL, 0DH   ; ASCII(enter) = 0D 
+        JE equalEnter ; If the entered value is ENTER, jump to equalEnter
             
         CMP AL, 2EH ; ASCII(dot<.>) = 2E  
         JA above2E  ; If the entered value is greater than 2E, jump to above2E
         
         CMP BYTE PTR [BP-20], AL
         JE equalPoint ; If the entered two value equal, jump to equalPoint (ONLY ABOVE 2E VALUE) 
+        
         MOV BYTE PTR [BP-20], AL
         LOOP label
-
+        JMP continue ; If the entered number reaches the max digit, jump to continue 
+        
     above2E: ; ASCII(0-9 number) = 30-39 / If the number is entered, proceed without checking
         MOV BYTE PTR [BP-20], AL
         SUB AL,30H
@@ -44,7 +50,15 @@ START:
         LEA DX,MSG2
         MOV AH,9
         INT 21H
-        END START
+        
+    equalEnter:
+        LEA DX,MSG3
+        MOV AH,9
+        INT 21H
+        JMP continue
+        ; programin devami burda olmali
+        
+    continue: ; End of user input and next process.(fibo) 
                 
 END START
                                    
