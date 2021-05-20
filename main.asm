f0623bf9d4bf9bbd3f6cfa3d15c66d7027942432
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-05-20 22:52:36 +0300

    Generation of fibonacci number up to input value.

--- main.asm
+++ main.asm
@@ -4,11 +4,13 @@
 ; EXAMPLE INPUT: 1.123
 DATA SEGMENT
     DIGIT DW 0 ; Number of digits 
-    PreviousValue DW 10
+    PreviousValue DW 10 
+    INPUTVALUE DW 0
     MSG1 DB 10,13,"Please enter a number (Max: 65.536 <2^16>)...: $"
     MSG2 DB 10,13,"ERROR! <Two points cannot be made one after the other.> $"   
     MSG3 DB 10,13,"Input value received... $"
-    MSG4 DB 10,13,"FIBO continuation... $"
+    MSG4 DB 10,13,"FIBO continuation... $" 
+    MSG5 DB 10,13,"The limit has been reached! $"
 ENDS
 
 
@@ -95,11 +97,82 @@ START:
         
      fiboLabel:
         ADD WORD PTR [BP-36],BX ; Let's add the value 10 ^ 0.
-        LEA DX,MSG4
+        XCHG BX,WORD PTR [BP-36] 
+        MOV INPUTVALUE, BX
         
+        LEA DX,MSG4
+        ; print screen
         MOV AH,9
         INT 21H
+        CALL FIBO
+        
+     fiboDone:       
+        LEA DX,MSG5
+        ; print screen
+        MOV AH,9
+        INT 21H
+        
+        JMP endOfFibonacci
             
-                    
+; start of process
+FIBO PROC NEAR     
+    MOV SI, 1  ; Fibonacci initial value
+    MOV DI, 0  ; Fibonacci(n-1) n = 1
+    MOV DI, 1  ; Fibonacci(n)   n = 1
+
+    MOV CX, 1  ; 10^n_digits  
+        
+    loopA:     
+        MOV AX, SI   ; Prepare for counting digits
+        MOV BX, 10   ; Print as decimal
+        
+    loopDigit:  
+        XCHG AX, CX  ; Calculate 10^n_digits
+        MUL  BX
+        XCHG CX, AX
+        XOR  DX, DX  
+        DIV  BX
+        OR   AX, AX  ; Test if the quotient is 0
+        JNZ  loopDigit
+
+        MOV  BX, SI
+        MOV  BP, 10
+        
+    loopPrint:  
+        MOV  AX, CX   ; Need to divide cx by 10 first
+        XOR  DX, DX
+        DIV  BP
+        MOV  CX, AX
+        MOV  AX, BX   ; Get the number
+        XOR  DX, DX
+        DIV  CX
+        MOV  BX, DX   ; The next number to be printed 
+        MOV  DL, AL   ; The quotient, current digit 
+        ; Every digit -DL-
+        ADD  DL, 30H  ; convert to ASCII
+        MOV  AH, 2H       
+            
+        ; control place SI ???
+         
+        INT  21H
+        CMP  CX, 1       ; Should end? - ZF is triggered when flag is 1!
+        ; Next case: JMP Division by 2 rule  
+        JNZ  loopPrint
+
+        ADD  DI, SI      ; Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+        JC   done        ; Overflow, done!
+        XCHG DI, SI      ; To keep them in order; si = F(n); di = F(n-1)
+        
+        ;Comparison of input value and fibonacci number.
+        CMP SI,InputValue ; first input > second input
+        JG  fiboDone
+        
+        JMP  loopA
+    done:   
+FIBO ENDP           
+; end of process
+
+
+       endOfFibonacci:            
 END START
                                    
